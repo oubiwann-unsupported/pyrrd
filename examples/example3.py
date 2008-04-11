309ba79fbcdf7a1d8a1a20e74deea638fc7829d8
duncan.mcgreggor <>
2008-04-11 21:23:15 +0000

    * Updated example3.

--- examples/example3.py
+++ examples/example3.py
@@ -6,10 +6,10 @@ from pyrrd.graph import DEF, CDEF, VDEF
 from pyrrd.graph import LINE, AREA, GPRINT
 from pyrrd.graph import ColorAttributes, Graph
 
-example_no = 3
-filename = 'example%s.rrd' % example_no
-graphfile = 'example%s.png' % example_no
-graphfile_lg = 'example%s-large.png' % example_no
+exampleNum = 3
+filename = 'example%s.rrd' % exampleNum
+graphfile = 'example%s.png' % exampleNum
+graphfile_lg = 'example%s-large.png' % exampleNum
 
 day = 24 * 60 * 60
 week = 7 * day
@@ -18,41 +18,41 @@ quarter = month * 3
 half = 365 * day / 2
 year = 365 * day
 
-start_time = 1122876000
-end_time = 1136012400
+startTime = 1122876000
+endTime = 1136012400
 step = 300
-max_steps = int((end_time-start_time)/step)
+maxSteps = int((endTime-startTime)/step)
 
 # Let's create and RRD file and dump some data in it
 dss = []
 rras = []
-ds1 = DS(ds_name='speed', ds_type='GAUGE', heartbeat=900)
+ds1 = DS(dsName='speed', dsType='GAUGE', heartbeat=900)
 dss.append(ds1)
 rra1 = RRA(cf='AVERAGE', xff=0.5, steps=24, rows=1460)
 rras.append(rra1)
 
-my_rrd = RRD(filename, ds=dss, rra=rras, start=start_time)
-my_rrd.create()
+myRRD = RRD(filename, ds=dss, rra=rras, start=startTime)
+myRRD.create()
 
 # let's generate some data...
-current_time = start_time
-for i in xrange(max_steps):
-    current_time += step
+currentTime = startTime
+for i in xrange(maxSteps):
+    currentTime += step
     # lets update the RRD/purge the buffer ever 100 entires
-    if i % 100 == 0 and my_rrd.values:
+    if i % 100 == 0 and myRRD.values:
         #print "updating RRD..."
-        my_rrd.update(debug=False)
+        myRRD.update(debug=False)
     # let's do periodic values
     value = int(sin(i % 200) * 1000)
-    my_rrd.bufferValue(current_time, value)
+    myRRD.bufferValue(currentTime, value)
 # add anything remaining in the buffer
-my_rrd.update()
+myRRD.update()
 
 # Let's set up the objects that will be added to the graph
-def1 = DEF(rrdfile=my_rrd.filename, vname='myspeed', ds_name=ds1.name)
+def1 = DEF(rrdfile=myRRD.filename, vname='myspeed', dsName=ds1.name)
 vdef1 = VDEF(vname='myavg', rpn='%s,AVERAGE' % def1.vname)
-area1 = AREA(def_obj=def1, color='#FFA902', legend='Raw Data')
-line1 = LINE(def_obj=vdef1, color='#01FF13', legend='Average', stack=True)
+area1 = AREA(defObj=def1, color='#FFA902', legend='Raw Data')
+line1 = LINE(defObj=vdef1, color='#01FF13', legend='Average', stack=True)
 
 # Let's configure some custom colors for the graph
 ca = ColorAttributes()
@@ -67,12 +67,12 @@ ca.font = '#FFFFFF'
 ca.arrow = '#FFFFFF'
 
 # Now that we've got everything set up, let's make a graph
-start_time = end_time - 3 * month
-g = Graph(graphfile, start=start_time, end=end_time, vertical_label='data', color=ca)
+startTime = endTime - 3 * month
+g = Graph(graphfile, start=startTime, end=endTime, vertical_label='data', color=ca)
 g.data.extend([def1, vdef1, area1])
 g.write()
 
-g = Graph(graphfile_lg, start=start_time, end=end_time, vertical_label='data', color=ca)
+g = Graph(graphfile_lg, start=startTime, end=endTime, vertical_label='data', color=ca)
 g.width = 800
 g.height = 400
 g.data.extend([def1, vdef1, area1])
