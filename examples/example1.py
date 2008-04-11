4fbb4381e2ecd4c0c8be3da6bc0fc5afcd7e2bbd
duncan.mcgreggor <>
2008-04-11 20:06:34 +0000

    * Updated vars in example 1.

--- examples/example1.py
+++ examples/example1.py
@@ -3,9 +3,9 @@ from pyrrd.graph import DEF, CDEF, VDEF
 from pyrrd.graph import LINE, AREA, GPRINT
 from pyrrd.graph import ColorAttributes, Graph
 
-example_no = 1
-filename = 'example%s.rrd' % example_no
-graphfile = 'example%s.png' % example_no
+exampleNum = 1
+filename = 'example%s.rrd' % exampleNum
+graphfile = 'example%s.png' % exampleNum
 
 # Let's create and RRD file and dump some data in it
 dss = []
@@ -15,24 +15,24 @@ dss.append(ds1)
 rra1 = RRA(cf='AVERAGE', xff=0.5, steps=1, rows=24)
 rra2 = RRA(cf='AVERAGE', xff=0.5, steps=6, rows=10)
 rras.extend([rra1, rra2])
-my_rrd = RRD(filename, ds=dss, rra=rras, start=920804400)
-my_rrd.create()
-my_rrd.bufferValue('920805600', '12363')
-my_rrd.bufferValue('920805900', '12363')
-my_rrd.bufferValue('920806200', '12373')
-my_rrd.bufferValue('920806500', '12383')
-my_rrd.bufferValue('920806800', '12393')
-my_rrd.bufferValue('920807100', '12399')
-my_rrd.bufferValue('920807400', '12405')
-my_rrd.bufferValue('920807700', '12411')
-my_rrd.bufferValue('920808000', '12415')
-my_rrd.bufferValue('920808300', '12420')
-my_rrd.bufferValue('920808600', '12422')
-my_rrd.bufferValue('920808900', '12423')
-my_rrd.update()
+myRRD = RRD(filename, ds=dss, rra=rras, start=920804400)
+myRRD.create()
+myRRD.bufferValue('920805600', '12363')
+myRRD.bufferValue('920805900', '12363')
+myRRD.bufferValue('920806200', '12373')
+myRRD.bufferValue('920806500', '12383')
+myRRD.bufferValue('920806800', '12393')
+myRRD.bufferValue('920807100', '12399')
+myRRD.bufferValue('920807400', '12405')
+myRRD.bufferValue('920807700', '12411')
+myRRD.bufferValue('920808000', '12415')
+myRRD.bufferValue('920808300', '12420')
+myRRD.bufferValue('920808600', '12422')
+myRRD.bufferValue('920808900', '12423')
+myRRD.update()
 
 # Let's set up the objects that will be added to the graph
-def1 = DEF(rrdfile=my_rrd.filename, vname='myspeed', ds_name=ds1.name)
+def1 = DEF(rrdfile=myRRD.filename, vname='myspeed', ds_name=ds1.name)
 cdef1 = CDEF(vname='kmh', rpn='%s,3600,*' % def1.vname)
 cdef2 = CDEF(vname='fast', rpn='kmh,100,GT,kmh,0,IF')
 cdef3 = CDEF(vname='good', rpn='kmh,100,GT,0,kmh,IF')
