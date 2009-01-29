a64014eefd914c0c27036cdce77cdb0dca5384a6
Duncan McGreggor <duncan@canonical.com>
2009-01-28 21:51:31 -0600

    * Updated commit script to run test runner.

--- admin/commit.sh
+++ admin/commit.sh
@@ -9,11 +9,11 @@ if [[ "$1" == "$FLAG" ]];then
 else
     # send the output (stdout and stderr) to both a file for checking and
     # stdout for immediate viewing/feedback purposes
-    trial $LIB 2>&1|tee test.out
+    python test/test_all.py 2>&1|tee test.out
     ./admin/checkBuild.sh || error
 fi
-STATUS=`tail -1 test.out|grep 'FAIL'`
-if [[ "$STATUS" == '' ]];then
+STATUS=`tail -1 test.out|awk '{print $1}'`
+if [[ "$STATUS" == "OK" ]];then
     if [[ "$1" == "FLAG" ]];then
         echo "Skipping tests..."
     else
